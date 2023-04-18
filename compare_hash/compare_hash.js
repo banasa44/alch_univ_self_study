9a99591c33276c0c8cec218167bd63e94fc9265e
banasa44 <cfbennassar@gmail.com>
2023-04-18 13:25:24 +0200

    v3 using array.find() method

--- compare_hash/compare_hash.js
+++ compare_hash/compare_hash.js
@@ -4,9 +4,6 @@ exports.findColor = void 0;
 const crypto_js_1 = require("crypto-js");
 // given a hash, return the color that created the hash
 function findColor(hash, COLORS) {
-    const index = COLORS
-        .map(str => (0, crypto_js_1.SHA256)(crypto_js_1.enc.Utf8.parse(str)))
-        .findIndex(h => h.toString() === hash);
-    return index !== -1 ? COLORS[index] : null;
+    return COLORS.find(x => (0, crypto_js_1.SHA256)(crypto_js_1.enc.Utf8.parse(x)).toString(crypto_js_1.enc.Hex) === hash);
 }
 exports.findColor = findColor;
--- compare_hash/test.ts
+++ compare_hash/test.ts
@@ -6,7 +6,7 @@ const hash_bad = 'ede4c8e51da3af3a85f245e144ebe7c8cafe231c20b1b9d13f18a8e293b3c3
 const hash_good = 'b1f51a511f1da0cd348b8f8598db32e61cb963e5fc69e2b41485bf99590ed75a';
 
 
-const color: string | null = findColor(hash_good, COLORS);
+const color: string | undefined = findColor(hash_good, COLORS);
 if (color) {
   console.log(`The color that created the hash_good ${hash_good} is ${color}.`);
 } else {
